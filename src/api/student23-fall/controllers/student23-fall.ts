3859d00289df03bd3d326f483df4c77dec2856f3
Dream4ever <cnhwvista@gmail.com>
2023-07-27 10:15:02 +0800

    feat: 添加教材中心供书项目的学生表

---
+++ src/api/student23-fall/controllers/student23-fall.ts
@@ -0,0 +1,7 @@
+/**
+ * student23-fall controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::student23-fall.student23-fall');
---
+++ src/api/student23-fall/services/student23-fall.ts
@@ -0,0 +1,7 @@
+/**
+ * student23-fall service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::student23-fall.student23-fall');
