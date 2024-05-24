37d80c11544c36ff6e36cd2a3529d08e2a98a8c9
Dream4ever <cnhwvista@gmail.com>
2024-05-24 09:20:27 +0800

    chore: 更新云读平台字段信息

--- upgrade.sh
+++ upgrade.sh
@@ -2,14 +2,18 @@
 
 CONTAINER_NAME=strapi
 
+# 停止当前的 Strapi 容器
 docker stop $CONTAINER_NAME
 printf 'container stopped\n'
 
+# 删除当前的 Strapi 容器
 docker rm $CONTAINER_NAME
 printf 'container deleted\n'
 
+# 删除当前的 Strapi 镜像
 docker image rm $CONTAINER_NAME
 printf 'image deleted\n'
 
+# 重新构建 Strapi 镜像
 docker compose -f compose-remote.yml up -d
 printf 'done\n'
