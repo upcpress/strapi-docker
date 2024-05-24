0934328e8e2a5e76215397b78d96991ec3a343d1
Dream4ever <cnhwvista@gmail.com>
2024-05-24 09:20:08 +0800

    feat: 新增云读平台所需表

---
+++ src/api/yd-order/controllers/yd-order.ts
@@ -0,0 +1,7 @@
+/**
+ * yd-order controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::yd-order.yd-order');
---
+++ src/api/yd-order/routes/yd-order.ts
@@ -0,0 +1,7 @@
+/**
+ * yd-order router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::yd-order.yd-order');
